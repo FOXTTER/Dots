19c899174a06440fb2481e901d673e7852a16149
Christian Witting <witting.chr@gmail.com>
2014-03-26 23:48:25 +0100

    Initial commit

---
+++ Dots/src/com/witting/dots/Ball.java
@@ -0,0 +1,58 @@
+package com.witting.dots;
+
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+
+public class Ball {
+	public float x;
+	public float y;
+	private float xSpeed;
+	private float ySpeed;
+	public float r;
+	private static float width = 800;
+	private static float height = 1600;
+	private Paint color = new Paint();
+	public Ball(float x, float y, float r, int col){
+		this.x = x;
+		this.y = y;
+		this.r = r;
+		this.color.setColor(col);
+		
+		xSpeed = 0;
+		ySpeed = 0;
+	}
+	public Ball(){
+		this(40 + (float)Math.random()*height-50,40+(float)Math.random()*width-50, 40,Color.RED);
+		Boolean selector = 0.5 < Math.random();
+		xSpeed = (selector ? 0 : 10);
+		ySpeed = (!selector ? 0 : 10);
+	}
+	public void draw(Canvas c){
+		if (CollisionBorder()) {
+			reverse();
+		}
+		x+=xSpeed;
+		y+=ySpeed;
+		c.drawCircle(x, y, r, color);
+	}
+	private float getDistance(float x1, float y1, float x2, float y2){
+		return (float)Math.sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2));
+	}
+	public boolean checkCollision(Ball bold){
+		return getDistance(this.x,this.y,bold.x,bold.y)< (this.r+bold.r);
+	}
+	public boolean CollisionBorder(){
+		if (x <= r && xSpeed < 0 || x >= width-r && xSpeed > 0) {
+			return true;
+		}
+		if (y <= r && ySpeed < 0 || y >= height-r && ySpeed > 0) {
+			return true;
+		}
+		return false;
+	}
+	private void reverse(){
+		xSpeed = -xSpeed;
+		ySpeed = -ySpeed;
+	}
+}
---
+++ Dots/src/com/witting/dots/GameScreen.java
@@ -0,0 +1,86 @@
+package com.witting.dots;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Paint.Align;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.MotionEvent.PointerCoords;
+import android.view.View;
+import android.widget.ImageView;
+
+public class GameScreen extends ImageView {
+	private Ball goal = new Ball(0,0,50,Color.BLUE);
+	private Ball player = new Ball(0,0,50,Color.GREEN);
+	List<Ball> list = new ArrayList();
+	public GameScreen(Context context, AttributeSet attrs){
+		super(context, attrs);
+		
+		
+		this.setOnTouchListener(new View.OnTouchListener() {
+			
+			@Override
+			public boolean onTouch(View v, MotionEvent event) {
+			    int eventaction = event.getActionMasked();
+				PointerCoords pos = new PointerCoords();
+			    switch (eventaction) {
+			    case MotionEvent.ACTION_DOWN:
+			    	invalidate();
+			    	break;
+			    case MotionEvent.ACTION_POINTER_DOWN: 
+			    	break;
+		        case MotionEvent.ACTION_POINTER_UP:		        	
+		            break;
+		        case MotionEvent.ACTION_UP:   
+		            break;
+				case MotionEvent.ACTION_MOVE:
+					for (int i = 0; i < event.getHistorySize(); i++) {
+						event.getHistoricalPointerCoords(0, i, pos);
+						player.x= pos.x;
+						player.y = pos.y;
+					}
+					
+					break;
+			    }
+			    return true; 
+			}
+		});
+	}
+	public Ball spawnEnemy(){
+		return new Ball();
+	}
+	Paint p = new Paint();
+	protected void onDraw(Canvas c) {
+		if (player.checkCollision(goal)) {
+			goal.x = (float)Math.random()*800;
+			goal.y = (float)Math.random()*1600;
+			list.add(spawnEnemy());
+		}
+		for (Ball enemy : list) {
+			enemy.draw(c);
+		}
+		goal.draw(c);
+		player.draw(c);
+		for (int i = 0; i < list.size(); i++) {
+			if (player.checkCollision(list.get(i))) {
+				
+				p.setColor(Color.RED);
+                p.setTextSize(100);
+                p.setTextAlign(Align.CENTER);
+                c.drawText("GAME OVER",400, 1000, p);
+                c.drawText("TOUCH TO START", 400, 500, p);
+                p.setTextAlign(Align.LEFT);
+                p.setTextSize(40);
+                list = new ArrayList<Ball>();
+                return;
+			}
+		}
+
+		invalidate();
+	}
+}
